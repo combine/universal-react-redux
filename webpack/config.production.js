c2522575c91cd04677d2830cd3bcd86f3e281723
Romain Dorgueil <romain@dorgueil.net>
2016-04-05 15:06:47 +0200

    chg: production webpack configuration should not point to webpack dev server, and should probably include a hash in the filename to avoid browser cache, and also allow to use them aggressively.

--- webpack/config.production.js
+++ webpack/config.production.js
@@ -3,8 +3,7 @@ var webpack    = require('webpack');
 var outputPath = 'dist/';
 var port       = process.env.PORT || 3001;
 var hostname   = process.env.HOSTNAME || 'localhost';
-var host       = 'http://' + hostname + ':' + port;
-var assetHost  = process.env.ASSET_HOST || host + '/' + outputPath;
+var assetHost  = process.env.ASSET_HOST || '/' + outputPath;
 var common     = require('./config.common');
 var baseUrl    = '/';
 var isoConfig  = require('./config.isomorphic');
@@ -15,26 +14,28 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var IsomorphicPlugin  = require('webpack-isomorphic-tools/plugin');
 
 module.exports = {
-  devServerPort: port,
   devtool: 'cheap-module-source-map',
   context: path.resolve(__dirname, '..'),
   entry: ['./client/index'],
   output: {
     path: path.join(__dirname, ('../' + outputPath)),
-    filename: 'bundle.js',
+    filename: '[name].[hash].js',
     publicPath: assetHost
   },
   plugins: [
-    new ExtractTextPlugin('styles.css'),
+    new webpack.optimize.DedupePlugin(),
+    new ExtractTextPlugin('[name].[hash].css'),
     new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en|es/),
     new IsomorphicPlugin(isoConfig),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        warnings: false
+        screw_ie8: true,
+        warnings: false,
       }
     }),
+    new webpack.optimize.AggressiveMergingPlugin(),
     new CompressionPlugin({
-      asset: '{file}.gz',
+      asset: '[file].gz',
       algorithm: 'gzip',
       test: /\.css$|\.js$|\.html$/,
       threshold: 10240,
