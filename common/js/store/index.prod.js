98b6432e794e01d2a1db86f46a18fc543969e30c
Kartik Lad <kartik@kartiklad.com>
2016-11-25 11:02:20 +1100

    removing redux-localstorage

--- common/js/store/index.prod.js
+++ common/js/store/index.prod.js
@@ -1,16 +1,8 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from 'reducers';
-import persistState from 'redux-localstorage';
 
-/* localStorage Persisted States
- * Set up persisted state properties via localStorage. They should be added
- * by their property name of the piece of state you want to persist, e.g:
- * const persistedStates = ['session', 'order'];
- */
-const persistedStates = [];
-
-export default function configureStore(initialState, localStorage = true) {
+export default function configureStore(initialState) {
   /* Middleware
    * Configure this array with the middleware that you want included. thunk
    * is included by default, and react-router-redux's syncHistory is also
@@ -21,13 +13,6 @@ export default function configureStore(initialState, localStorage = true) {
   // Add universal enhancers here
   let enhancers = [];
 
-  // Client-side enhancers and middleware
-  if (isBrowser()) {
-    if (localStorage) {
-      enhancers.push(persistState(persistedStates));
-    }
-  }
-
   const enhancer = compose(...[
     applyMiddleware(...middleware),
     ...enhancers
@@ -37,8 +22,4 @@ export default function configureStore(initialState, localStorage = true) {
   const store = createStore(rootReducer, initialState, enhancer);
 
   return store;
-}
-
-function isBrowser() {
-  return (typeof window !== 'undefined' && typeof window.document !== 'undefined');
 }
