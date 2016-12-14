ee7c0d16c9d14b7371c0d635a580a197fc3b4fe6
Calvin Lai <callai@gmail.com>
2016-12-14 12:36:18 -0500

    Add better-npm-run and update run scripts, resolves #26.

---
+++ postinstall.js
@@ -0,0 +1,15 @@
+const exec = require('child_process').exec;
+
+// For cross-platform postinstalls, use a node script to execute any
+// post-install tasks rather than using a bash script since that wouldn't work
+// on Windows.
+if (process.env.NODE_ENV === 'production') {
+  exec('yarn run prod:build', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
+}
