eb8281ac4a959b5d8141cb666f136ca25872693d
Calvin Lai <callai@gmail.com>
2017-06-26 14:43:23 -0400

    Remove <Main> component and just use App

--- common/js/containers/App/index.js
+++ common/js/containers/App/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import Main from 'components/Main';
+import { Switch, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
+import routes from 'routes';
 
 const App = () => (
-  <div>
+  <Container fluid={false}>
     <Header />
-    <Main />
+    <Switch>
+      {routes.map(route => <Route key={route.path} {...route} />)}
+    </Switch>
     <Footer />
-  </div>
+  </Container>
 );
 
 export default App;
