81feb7fb36a53faa4c4f91547d3048c79b4d35c0
Calvin Lai <callai@gmail.com>
2018-01-09 15:47:39 -0500

    feat: remove AppContainer from RHL3, mark App container as hot-exported.

--- client/index.js
+++ client/index.js
@@ -3,39 +3,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
-import { AppContainer } from 'react-hot-loader';
 import createHistory from 'history/createBrowserHistory';
 import configureStore from 'store';
 import App from 'containers/App';
 
-const history = createHistory();
-
-// The root element of your app
-const rootElement = document.getElementById('app');
-
-// Creates the Redux store based on the initial state passed down by the server
-// rendering.
+// Hydrate the redux store from server state.
 const initialState = window.__INITIAL_STATE__;
+const history = createHistory();
 const store = configureStore(initialState, history);
 
-const render = (Component) => {
-  ReactDOM.hydrate(
-    <Provider store={store}>
-      <AppContainer>
-        <ConnectedRouter history={history}>
-          <Component />
-        </ConnectedRouter>
-      </AppContainer>
-    </Provider>,
-    rootElement
-  );
-};
-
-render(App);
-
-if (module.hot) {
-  // We need to re-require the main App module.
-  module.hot.accept('../common/js/containers/App', () => {
-    render(require('../common/js/containers/App').default);
-  });
-}
+// Render the application
+ReactDOM.hydrate(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('app')
+);
